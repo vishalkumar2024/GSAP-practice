d4ad205929c96d2540a5a6dfff57eddc7dcfa56f
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-12-20 13:21:57 +0530

    basic animation

--- React-Gsap/src/App.jsx
+++ React-Gsap/src/App.jsx
@@ -1,12 +1,32 @@
-
 import './App.css'
+import { gsap } from 'gsap'
+import { useGSAP } from '@gsap/react';
+import { useRef } from 'react';
 
 function App() {
+  const secDiv = useRef()
+
+  useGSAP(() => {
+    gsap.to("#box1", {
+      x: 1000,
+      delay:1,
+      duration: 2,
+      backgroundColor: "green",
+    })
+
+    gsap.from(secDiv.current,{
+      x: 1000,
+      delay:1,
+      duration: 2,
+      backgroundColor: "green",
+    })
+  })
 
   return (
-    <>
-
-    </>
+    <div>
+      <div id="box1">This is 1st div</div>
+      <div ref={secDiv} id="box2">This is 2nd div</div>
+    </div>
   )
 }
 
