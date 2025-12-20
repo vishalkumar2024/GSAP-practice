fd9d5f1b248e94887c1748d12ccb0c3e422103e4
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-12-20 13:54:36 +0530

    understand dependencies with useState

--- React-Gsap/src/App.jsx
+++ React-Gsap/src/App.jsx
@@ -1,31 +1,59 @@
 import './App.css'
 import { gsap } from 'gsap'
 import { useGSAP } from '@gsap/react';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 function App() {
   const secDiv = useRef()
+  const [random, setRandom] = useState(0);
+  const [roter, setRoter] = useState(0);
+
+  const Random = gsap.utils.random(-200, 300, 10);
+  const roters = gsap.utils.random(-360, 720, 30)
 
   useGSAP(() => {
     gsap.to("#box1", {
       x: 1000,
-      delay:1,
+      delay: 1,
       duration: 2,
+      repeat: -1,
       backgroundColor: "green",
     })
 
-    gsap.from(secDiv.current,{
+    gsap.from(secDiv.current, {
       x: 1000,
-      delay:1,
+      delay: 1,
       duration: 2,
+      repeat: -1,
       backgroundColor: "green",
     })
   })
 
+  useGSAP(() => {
+    gsap.to("#box", {
+      x:Random,
+      duration:1,
+      rotate:roters,
+    })
+  },[random, roter ])
+
+   
   return (
     <div>
-      <div id="box1">This is 1st div</div>
-      <div ref={secDiv} id="box2">This is 2nd div</div>
+
+      <div>
+        <div id="box1">This is 1st div</div>
+        <div ref={secDiv} id="box2">This is 2nd div</div>
+      </div>
+
+      <div className="secondDiv">
+        <div id="box"></div>
+        <button id="btn" onClick={() => {
+          setRandom(Random)
+         setRoter(roters)
+        }}>Animate</button>
+      </div>
+
     </div>
   )
 }
